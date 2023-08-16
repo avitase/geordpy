9519f8a0fb044bf080919a2901c1f0a597a28d84
Meinert, Nis <Nis.Meinert@dlr.de>
2023-08-16 16:36:51 +0200

    Update documentation

--- README.md
+++ README.md
@@ -3,12 +3,7 @@
 [![PyPI](https://img.shields.io/pypi/v/geordpy)](https://pypi.org/project/geordpy/)
 [![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 
-GeoRDPy is a Python library that simplifies geodetic-coordinate polylines using the Ramer-Douglas-Peucker algorithm. It ensures accuracy by utilizing the precise distance calculations from the GeographicLib by Charles Karney on the WGS 84 reference ellipsoid.
-
-## Features
-- Simplify geodetic-coordinate polylines using the Ramer-Douglas-Peucker algorithm.
-- Utilize precise distance calculations with GeographicLib on the WGS 84 reference ellipsoid.
-- Easy-to-use interface with a single function call.
+GeoRDPy is a Python library that simplifies geodetic-coordinate polylines using the Ramer-Douglas-Peucker algorithm. It utilizes the distance to great circle segments as the distance metric to reduce the number of points in a polyline while maintaining accuracy.
 
 ## Installation
 GeoRDPy releases are available as wheel packages for macOS, Windows and Linux on [PyPI](https://pypi.org/project/geordpy/).
@@ -21,13 +16,10 @@ $ pip install geordpy
 ```python
 >>> import numpy as np
 >>> import geordpy
->>> points = np.array([(42.0, -75.0), (42.1, -74.9), (42.2, -75.1), (42.3, -74.8)])
+>>> points = np.array([...])
 >>> threshold = 15_000  # meters
->>> mask = geordpy.filter(points, threshold)
->>> points[mask]
-array([[ 42. , -75. ],
-       [ 42.2, -75.1],
-       [ 42.3, -74.8]])
+>>> mask = geordpy.rdp_filter(points, threshold)
+>>> points = points[mask]  # filter points with mask
 ```
 
 For more details, check the [documentation](https://avitase.github.io/geordpy/).
--- geordpy/geordpy.py
+++ geordpy/geordpy.py
@@ -30,6 +30,36 @@ def _filter(points, threshold):
 
 
 def rdp_filter(points, threshold, radius=6_371_000):
+    """
+    Simplify a geodetic-coordinate polyline using the Ramer-Douglas-Peucker algorithm.
+
+    This function applies the Ramer-Douglas-Peucker (RDP) algorithm to a list of geodetic-coordinate points,
+    aiming to simplify the polyline while keeping the error below a specified threshold. The algorithm
+    works by approximating the original polyline with a reduced number of points that lie close to the
+    original curve.
+
+    Args:
+        points (list of tuple): A list of latitude and longitude pairs (given in degrees) representing the
+                               geodetic-coordinate polyline points to be simplified.
+
+        threshold (float): The maximum allowable error, specified as an arc length along great circle segments.
+                          Points that deviate from the simplified curve by more than this threshold will be kept.
+
+        radius (float, optional): The radius of the sphere used for calculations. Defaults to Earth's mean radius
+                                 in meters (6371000). The threshold is interpreted based on this radius.
+
+    Returns:
+        list of bool: A binary mask indicating whether each point in the input list should be kept or discarded
+                      based on the RDP simplification. The mask has the same length as the input 'points' list,
+                      where a value of True indicates that the corresponding point should be kept, and False
+                      indicates that the point can be discarded.
+
+    Note:
+        - The input 'points' list should have at least two points to define a valid polyline.
+        - The 'threshold' value must be greater than zero.
+        - The 'radius' value should be set appropriately to match the units of the threshold. For example, if the
+          radius is set to 1, then the threshold corresponds to the arc length on the unit sphere.
+    """
     if len(points) == 0:
         return np.empty(0, dtype=bool)
 
